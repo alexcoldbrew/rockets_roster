cbf5da5cf29586cc18b7a8708d1c16391a72cab1
alexcoldbrew <62966330+alexcoldbrew@users.noreply.github.com>
2020-09-29 15:42:14 -0500

    created new project setup

---
+++ lib/rockets_roster.rb
@@ -0,0 +1,6 @@
+require "rockets_roster/version"
+
+module RocketsRoster
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rockets_roster/version.rb
@@ -0,0 +1,3 @@
+module RocketsRoster
+  VERSION = "0.1.0"
+end
